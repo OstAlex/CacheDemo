675a3157fce0bf707f96f65a147106ad0d1678f7
zuochuanqiang <zuochuanqiang@jd.com>
2018-08-27 11:08:13 +0800

    git push test

--- app/src/main/java/ooo/zuo/cachedemo/MainActivity.java
+++ app/src/main/java/ooo/zuo/cachedemo/MainActivity.java
@@ -18,6 +18,7 @@ import ooo.zuo.cachedemo.cache.CacheUtils;
 public class MainActivity extends AppCompatActivity {
 
     long time ;
+    // master v1
     char[] chars = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
